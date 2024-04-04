c9cd3fc96b4e3e4e5c63e6cc06fa692a268687d6
Serial CHiller <67603638+Wise-Dude@users.noreply.github.com>
2024-04-04 23:31:37 +0600

    connected mongodb atlast database to express js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,29 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 4000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const url = "mongodb+srv://UwUcoms:Nn8vFVCGX48Hd5pv@cluster0.1plpfax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function connectToMongoDB() {
+    try {
+      await client.connect();
+      console.log("Connected to MongoDB Atlas!");
+    } catch (error) {
+      console.error("Error connecting to MongoDB Atlas:", error);
+    }
+  }
+  
+  connectToMongoDB();
+    
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
